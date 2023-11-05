4cc74ed808fd82352ff596918f321dfa5084978e
Siret Jorro <siret.jorro@lab.mobi>
2023-11-05 17:59:36 +0200

    P42-218 add step to run project creator

---
+++ ci/script.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+bundle install
+bundle exec cap android:create creatorapp creatorapp
+cp ci/google-services.json creatorapp/app
+cp ci/debug.keystore ci/signing.keystore creatorapp/app/signing
+cd creatorapp
+chmod +x gradlew
+echo "sdk.dir=$ANDROID_SDK_ROOT" > "$FCI_BUILD_DIR/local.properties"
+cd app
+sed -i '/signingConfigs/,/buildTypes/c\    signingConfigs { \
+        releaseSigning { \
+            storeFile file("signing/signing.keystore") \
+	    storePassword $CM_KEYSTORE_PASSWORD_RELEASE \
+	    keyAlias $CM_KEY_ALIAS \
+            keyPassword $CM_KEY_PASSWORD_RELEASE \
+        } \
+        debugSigning { \
+            storeFile file("signing/release.keystore") \
+            storePassword $CM_KEYSTORE_PASSWORD_DEBUG \
+            keyAlias $CM_KEY_ALIAS \
+            keyPassword $CM_KEY_PASSWORD_DEBUG \
+        } \
+    } \
+ \
+    buildTypes {' build.gradle
--- script.rb
+++ script.rb
@@ -13,39 +13,49 @@ end
 
 namespace :android do
   task :ask_stuff do
-    set :output_folder, i.ask('Relative path to output? ')
+    if ARGV.length == 3
+      set :output_folder, ARGV[1]
+      project_name = ARGV[2]
+      sanitized_name = project_name.gsub(/[^A-Za-z0-9]/, '')
+      package = "mobi.lab.#{sanitized_name.downcase}"
+      set :project_name, project_name
+      set :sanitized_name, sanitized_name
+      set :package, package
+    else
+      set :output_folder, i.ask('Relative path to output? ')
 
-    project_name = nil
-    loop do
-      project_name = i.ask('Project name? (no trailing -android) ')
-      if project_name.empty?
-        puts "Name is required!"
-      else
-        break
-      end
-    end
-
-    sanitized_name = project_name.gsub(/[^A-Za-z0-9]/, '')
-    package = "mobi.lab.#{sanitized_name.downcase}"
-
-    ok = false
-    while not ok
-      puts "You have chosen the name #{project_name}"
-      puts "Current names are:\n"
-      puts "package:     #{package}"
-      puts "sanitized:   #{sanitized_name}"
-      puts "name:        #{project_name}"
-
-      i.choose do |menu|
-        menu.prompt = 'This looks ok to you?  '
-        menu.choice(:yes) do
-          puts 'Cool.'
-          ok = true
+      project_name = nil
+      loop do
+        project_name = i.ask('Project name? (no trailing -android) ')
+        if project_name.empty?
+          puts "Name is required!"
+        else
+          break
         end
-        menu.choice(:no) do
-          package = i.ask('Package: ') { |q| q.default = package }
-          sanitized_name = i.ask('Sanitized name: ') { |q| q.default = sanitized_name }
-          project_name = i.ask('Project name: ') { |q| q.default = project_name }
+      end
+
+      sanitized_name = project_name.gsub(/[^A-Za-z0-9]/, '')
+      package = "mobi.lab.#{sanitized_name.downcase}"
+
+      ok = false
+      while not ok
+        puts "You have chosen the name #{project_name}"
+        puts "Current names are:\n"
+        puts "package:     #{package}"
+        puts "sanitized:   #{sanitized_name}"
+        puts "name:        #{project_name}"
+
+        i.choose do |menu|
+          menu.prompt = 'This looks ok to you?  '
+          menu.choice(:yes) do
+            puts 'Cool.'
+            ok = true
+          end
+          menu.choice(:no) do
+            package = i.ask('Package: ') { |q| q.default = package }
+            sanitized_name = i.ask('Sanitized name: ') { |q| q.default = sanitized_name }
+            project_name = i.ask('Project name: ') { |q| q.default = project_name }
+          end
         end
       end
     end
