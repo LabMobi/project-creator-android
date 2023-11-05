190b98ce144529cc5c7e540041c405a0631a5fa2
Siret Jorro <siret.jorro@lab.mobi>
2023-11-05 18:59:57 +0200

    P42-218 fix spacing

--- ci/script.sh
+++ ci/script.sh
@@ -9,19 +9,19 @@ chmod +x gradlew
 echo "sdk.dir=$ANDROID_SDK_ROOT" > "$FCI_BUILD_DIR/local.properties"
 cd app
 sed -i "/signingConfigs {/,/buildTypes/c\ \
-    signingConfigs {\n \
-        releaseSigning {\n \
-            storeFile file("signing/signing.keystore")\n \
-            storePassword $CM_KEYSTORE_PASSWORD_RELEASE\n \
-            keyAlias $CM_KEY_ALIAS\n \
-            keyPassword $CM_KEY_PASSWORD_RELEASE\n \
-        }\n \
-        debugSigning {\n \
-            storeFile file("signing/release.keystore")\n \
-            storePassword $CM_KEYSTORE_PASSWORD_DEBUG\n \
-            keyAlias $CM_KEY_ALIAS\n \
-            keyPassword $CM_KEY_PASSWORD_DEBUG\n \
-        }\n \
-    }\n \
+	signingConfigs {\n \
+		releaseSigning {\n \
+			storeFile file("signing/signing.keystore")\n \
+			storePassword $CM_KEYSTORE_PASSWORD_RELEASE\n \
+			keyAlias $CM_KEY_ALIAS\n \
+			keyPassword $CM_KEY_PASSWORD_RELEASE\n \
+		}\n \
+		debugSigning {\n \
+			storeFile file("signing/release.keystore")\n \
+			storePassword $CM_KEYSTORE_PASSWORD_DEBUG\n \
+			keyAlias $CM_KEY_ALIAS\n \
+			keyPassword $CM_KEY_PASSWORD_DEBUG\n \
+		}\n \
+	}\n \
 \n \
-    buildTypes {" build.gradle
+	buildTypes {" build.gradle
