b016aabfe659b5b550c67af67cd795907c8b1170
lauris <lauris.kruusamae@lab.mobi>
2023-12-11 12:50:07 +0200

    Improve assistedViewModels extension function documentation and describe in more detail why they are needed

--- project/app/src/main/java/mobi/lab/sample/common/ViewModelExtensions.kt
+++ project/app/src/main/java/mobi/lab/sample/common/ViewModelExtensions.kt
@@ -9,12 +9,22 @@ import dagger.hilt.android.lifecycle.withCreationCallback
 import kotlin.reflect.KClass
 
 /**
- * Activity based Assisted Injection
+ * These extensions functions help with creating ViewModels with Assisted Injection factories.
+ * There is a new CreationExtras feature that allows to pass in a callback when the ViewModel needs to be created.
+ * HiltViewModels use this feature to provide a hook to invoke the custom AssistedInjection factory interface.
  *
+ * These extension functions are just convenience functions around the extrasProducer field and make the API less verbove.
+ *
+ * NB! Similar extension functions are available in the https://github.com/LabMobi/mvvm-android. In hindsight, it wasn't the best
+ * idea to include those extension functions into the MVVM library as they are specific to our project template's Dagger usage and
+ * not connected to MVVM pattern as such.
+ */
+
+/**
  * Convenience function to init ViewModels lazily using Assisted Injection.
  * Wraps the assisted factory invocation with creation extras.
  *
- * The extra class argument is needed so we can invoke the pr
+ * The extra assistedViewModelFactoryClass argument is needed so we can invoke the assistedViewModelFactory with a properly typed argument.
  */
 @Suppress("UNUSED_PARAMETER")
 inline fun <reified VM : ViewModel, VMF : Any> ComponentActivity.assistedViewModels(
@@ -29,10 +39,10 @@ inline fun <reified VM : ViewModel, VMF : Any> ComponentActivity.assistedViewMod
 }
 
 /**
- * Activity based Assisted Injection
- *
  * Convenience function to init ViewModels lazily using Assisted Injection.
  * Wraps the assisted factory invocation with creation extras.
+ *
+ * The extra assistedViewModelFactoryClass argument is needed so we can invoke the assistedViewModelFactory with a properly typed argument.
  */
 @Suppress("UNUSED_PARAMETER")
 inline fun <reified VM : ViewModel, VMF : Any> Fragment.assistedViewModels(
