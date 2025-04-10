b45a8123d844a35a69ff94d7139892e21ddd257d
Harri Kirik <harri.kirik@lab.mobi>
2025-04-10 11:22:42 +0300

    MW-95: Move projectlib to target API 36. Handle edge-to-edge

--- projectlib/sample/src/main/java/mobi/lab/sample/sample/MainActivity.kt
+++ projectlib/sample/src/main/java/mobi/lab/sample/sample/MainActivity.kt
@@ -1,15 +1,40 @@
 package mobi.lab.sample.sample
 
 import android.os.Bundle
+import android.widget.Button
 import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.updatePadding
 import mobi.lab.sample.SampleClass
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        enableEdgeToEdge()
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main)
+        setSupportActionBar(findViewById(R.id.my_toolbar))
 
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.content)) { v, insets ->
+            val bars = insets.getInsets(
+                WindowInsetsCompat.Type.systemBars()
+                    or WindowInsetsCompat.Type.displayCutout()
+            )
+            v.updatePadding(
+                left = bars.left,
+                top = bars.top,
+                right = bars.right,
+                bottom = bars.bottom,
+            )
+            WindowInsetsCompat.CONSUMED
+        }
+
+        findViewById<Button>(R.id.button).setOnClickListener { useLibrary() }
+    }
+
+    private fun useLibrary() {
         // Use our library here
         Toast.makeText(this, SampleClass().foo(), Toast.LENGTH_LONG).show()
     }
