e99a792ebca54f9570bf5f6ff34aa68742c87bb0
Harri Kirik <harri.kirik@lab.mobi>
2025-05-22 17:00:14 +0300

    Add the headers for https://reqres.in/. Update from master branch to main branch in the Codemagic

--- project/app-infrastructure/src/main/java/mobi/lab/sample/infrastructure/auth/remote/AuthResource.kt
+++ project/app-infrastructure/src/main/java/mobi/lab/sample/infrastructure/auth/remote/AuthResource.kt
@@ -2,10 +2,12 @@ package mobi.lab.sample.infrastructure.auth.remote
 
 import io.reactivex.rxjava3.core.Single
 import retrofit2.http.Body
+import retrofit2.http.Headers
 import retrofit2.http.POST
 
 interface AuthResource {
 
+    @Headers("x-api-key: reqres-free-v1")
     @POST("login")
     fun login(@Body request: ApiLoginRequest): Single<ApiSession>
 }
